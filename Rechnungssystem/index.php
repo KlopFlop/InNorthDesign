8330e708e13c27bf5595ec8b646b663eba740f8f
Lucas <lucas.merres@outlook.de>
2020-12-04 06:42:35 +0100

    framework

---
+++ Rechnungssystem/index.php
@@ -0,0 +1,72 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+
+</head>
+
+<!-- ########## CONTENT DATA ########## -->
+
+<body>
+    <div id="main_content">
+        <div id="section_data" class="secendary_content">
+            <div id="data_header" class="content_header">
+                <div class="header_shape_one"> </div>
+                <div class="header_shape_two"> </div>
+                <div class="header_text">
+                    <h2 class="number">1.</h2>
+                    <h2 class="catagory">Daten</h2>
+                </div>
+
+            </div>
+            <div class="content_content">
+                <form>
+                    <input type="Kundenname">
+
+                </form>
+            </div>
+        </div>
+
+<!-- ########## CONTENT ORDER ########## -->
+
+        <div id="section_order" class="secendary_content">
+            <div id="order_header" class="content_header">
+            <div class="header_shape_one">
+
+            </div>
+            <div class="header_shape_two">
+
+            </div>
+            <div class="header_text">
+              <h2 class="number">2.</h2>
+              <h2 class="catagory">Bestellung</h2>
+            </div>
+            </div>
+        </div>
+
+        <div id="section_payment" class="secendary_content">
+            <div id="payment_header" class="content_header">
+            <div class="header_shape_one">
+
+            </div>
+            <div class="header_shape_two">
+
+            </div>
+            <div class="header_text">
+            <h2 class="number">3.</h2>
+             <h2 class="catagory">Abrechnung</h2>
+            </div>
+            </div>
+        </div>
+
+        <div id="section_push" class="secendary_content">
+            <div id="push_header" class="content_header">
+                
+            </div>
+        </div>
+
+    </div>
+</body>
+</html>
